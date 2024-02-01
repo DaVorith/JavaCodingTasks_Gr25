f7e4b76f3135046e7e04cd7cf1bf7260a96ede73
Mefa99 <fatix.aydin@gmail.com>
2024-02-01 12:08:53 -0500

    week 1 solutions updated

--- src/week01/task2/DivideWithoutDivisionOperator_Fatih.java
+++ src/week01/task2/DivideWithoutDivisionOperator_Fatih.java
@@ -1,4 +1,19 @@
 package week01.task2;
 
 public class DivideWithoutDivisionOperator_Fatih {
+
+    public static void main(String[] args) {
+
+
+        divideWithoutOperator(14, 4);
+    }
+
+    public static void divideWithoutOperator(int num1, int num2) {
+        int count = 0;
+        while (num1 >= num2) {// 10 >= 6 true
+            num1 -= num2; // 10
+            count++;
+        }
+        System.out.println(count + " remainder is " + num1);
+    }
 }
