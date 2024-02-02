292d2284daff51c7f55d4b9cc02c36bd28215894
MerminaB <merminaborovina@hotmail.com>
2024-02-01 21:37:23 -0500

    Tasks week 1 finished

--- src/week01/task1/OddOrEven_Mermina.java
+++ src/week01/task1/OddOrEven_Mermina.java
@@ -1,7 +1,24 @@
 package week01.task1;
 
+import java.util.Scanner;
+
 public class OddOrEven_Mermina {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number" );
+        int num = scanner.nextInt();
+        if(num % 2 == 0) {
+            System.out.println(num + " is even" );
+        }else{
+            System.out.println(num + " is odd" );
+        }
+
+
+
     }
 }
+   /* Write a method that can identify  a given number is even or odd.
+        EX:
+        identify(5) --> "Odd"
+        identify(6) --> "Even" */
