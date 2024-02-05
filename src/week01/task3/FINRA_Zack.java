e616f5cdd1f4f05c50efd1ab522d047d49386062
Yaren <yarenboz6234@hotmail.com>
2024-02-05 10:33:06 -0500

    created FINRA task

--- src/week01/task3/FINRA_Zack.java
+++ src/week01/task3/FINRA_Zack.java
@@ -4,6 +4,22 @@ import java.util.Arrays;
 
 public class FINRA_Zack {
 
+    public static void main(String[] args) {
+        FINRA(40);
+    }
+
+    public static void FINRA(int num){
+        if(num%15==0){
+            System.out.println("FINRA");
+        } else if (num%3==0) {
+            System.out.println("FIN");
+        } else if (num%5==0) {
+            System.out.println("RA");
+        }else{
+            System.out.println(num);
+        }
+    }
+
     /*
      Write a function which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3,
      print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number.
