df37cc944f36fcc3ffbf5bba246ff278ff06070d
Yaren <yarenboz6234@hotmail.com>
2024-02-05 10:32:48 -0500

    created OddOrEven task

--- src/week01/task1/OddOrEven_Zack.java
+++ src/week01/task1/OddOrEven_Zack.java
@@ -2,14 +2,27 @@ package week01.task1;
 
 public class OddOrEven_Zack {
 
-    /*
+    public static void main(String[] args) {
+
+        oddOrEven(5);
+
+    }
+
+    public static void oddOrEven(int num) {
+
+        if (num % 2 == 0) {
+            System.out.println("identify(" + num + "); ->\"Even\"");
+        }else{
+            System.out.println("identify(" + num + "); ->\"Odd\"");
+        }
+
+    }
+}
+
+/*
      Numbers - Odd or even:
      Write  a method which can identify given number is even or odd
      Output ex:
      identify(5); ->"Odd"
      identify(6); ->"Even"
      */
-
-
-
-}
