c76c9e7baa389fc5d54d73aadeea0fb6eb9bbcd0
Admin <charosvarano@gmail.com>
2024-02-06 09:39:00 -0500

    creat  end  finsh week 02task

---
+++ src/wqeek02/PrintConsecutive.java
@@ -0,0 +1,74 @@
+package wqeek02;
+
+public class PrintConsecutive {
+
+    public static void printConsecutiveNumbers(int N) {
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("TestCoders");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 17;
+        printConsecutiveNumbers(N);
+    }
+}
+/*
+Explanation:
+
+The printConsecutiveNumbers function iterates through numbers from 1 to N.
+For each number, it checks if it's divisible by 2, 3, and/or 5 and prints the appropriate
+word(s) or the number itself accordingly.
+If a number is divisible by both 2 and 3, it prints "CodilityTest".
+If a number is divisible by both 2 and 5, it prints "CodilityCoders".
+If a number is divisible by both 3 and 5, it prints "TestCoders".
+If a number is divisible by 2, it prints "Codility".
+If a number is divisible by 3, it prints "Test".
+If a number is divisible by 5, it prints "Coders".
+Otherwise, it prints the number itself.
+*/
+
+/*
+ Numbers -- print consecutive numbers
+Write a function that, given a positive integer N, prints the consecutive numbers from 1 to N,
+ each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the
+ word
+  Codility, Test or Coders respectively. If a number is divisible by more than one of the
+   numbers: 2,3 or 5, it should be replaced by a concatenation of the respective words Codility,
+   Test and Coders in this given order. For example, numbers divisible by both 2 and 3 should be
+    replaced by CodilityTest and numbers divisible by all three numbers: 2,3 and 5, should be
+    replaced by CodilityTestCoders.
+For example, here is the output for N = 17:
+1
+Codility
+Test
+Codility
+Coders
+CodilityTest
+7
+Codility
+Test
+CodilityCoders
+11
+CodilityTest
+13
+Codility
+TestCoders
+Codility
+ */
