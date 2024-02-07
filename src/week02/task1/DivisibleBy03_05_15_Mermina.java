e5ae03ee75592ba0c3821b2fdd4041dd7b2c15d9
MerminaBorovina <Merminaborovina@hotmail.com>
2024-02-06 21:55:37 -0500

    tasks week finished

--- src/week02/task1/DivisibleBy03_05_15_Mermina.java
+++ src/week02/task1/DivisibleBy03_05_15_Mermina.java
@@ -3,9 +3,32 @@ package week02.task1;
 public class DivisibleBy03_05_15_Mermina {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println("\nDivisible By 15");
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 15 != 0) {
+                System.out.println(i + "");
+            }
+        }
+
+        System.out.println("\nDivisible By 5");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 15 != 0) {
+                System.out.print(i + " ");
+
+
+            }
+        }
+        System.out.println("\nDivisible By 5");
     }
 }
+
 /*
 Write a program that can print the numbers between 1 ~ 100 that can be divisible by 3, 5, and 15.
 -if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibelBy15 section
--- src/week02/task2/PrintConsecutiveNumbers_Mermina.java
+++ src/week02/task2/PrintConsecutiveNumbers_Mermina.java
@@ -3,8 +3,28 @@ public class PrintConsecutiveNumbers_Mermina {
     public static void main(String[] args) {
 
 
+        int N = 17; // Change N to any positive integer
+
+        for (int i = 1; i <= N; i++) {
+            String output = "";
+
+            if (i % 2 == 0) {
+                output += "Codility";
+            }
+            if (i % 3 == 0) {
+                output += "Test";
+            }
+            if (i % 5 == 0) {
+                output += "Coders";
+            }
+
+            System.out.println(output.isEmpty() ? i : output);
+        }
     }
 }
+
+
+
    /* Write a function that, given a positive integer N, prints the consecutive numbers from 1 to N, each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the word Codility, Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5, it should be replaced by a concatenation of the respective words Codility, Test and Coders in this given order. For example, numbers divisible by both 2 and 3 should be replaced by CodilityTest and numbers divisible by all three numbers: 2,3 and 5, should be replaced by CodilityTestCoders.
 
         For example, here is the output for N = 17:
