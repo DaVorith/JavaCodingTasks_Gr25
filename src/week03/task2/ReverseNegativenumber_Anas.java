ed82550a8397f102dbc14c3dbb9de5850f0e8830
Anasben00 <bendwamas@gmail.com>
2024-02-15 17:03:43 -0500

    rename class

--- src/week03/task2/ReverseNegativenumber_Anas.java
+++ src/week03/task2/ReverseNegativenumber_Anas.java
@@ -1,14 +1,16 @@
 package week03.task2;
 
-public class reverseNegativenumber_Anas {
+public class ReverseNegativenumber_Anas {
     public static void main(String[] args) {
-        System.out.println(NegativNum(-17));
+        System.out.println(NegativNum(-125));
     }
     public static int NegativNum(int num){
         String number ="";
+
         for (int i = String.valueOf(num).length()-1; i >0 ; i--) {
-            number+=String.valueOf(num).charAt(i);
+            number+= String.valueOf(num).charAt(i);
         }
         return -Integer.parseInt(number);
     }
+
 }
