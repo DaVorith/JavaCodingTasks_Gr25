0f967c012e269adaf2ae214a4785fd885e41cce8
Anasben00 <bendwamas@gmail.com>
2024-02-27 18:53:48 -0500

    UPdat freq Of char

--- src/week04/task1/FrequencyofCharacters_Anas.java
+++ src/week04/task1/FrequencyofCharacters_Anas.java
@@ -10,15 +10,19 @@ public class FrequencyofCharacters_Anas {
 
         for (int i = 0; i < word.length(); i++) {
             int c=0;
-            for (int j = 0; j < word.length(); j++) {
-                if (word.charAt(i)==word.charAt(j)){
-                    c++;
+            if (result.contains(""+word.charAt(i))){
+                continue;
+            }
+                for (int j = 0; j < word.length(); j++) {
+                    if (word.charAt(i) == word.charAt(j)) {
+                        c++;
+                    }
                 }
-            }
-            if (!result.contains(""+word.charAt(i))){
-                result+= ""+word.charAt(i)+c;
+                if (!result.contains("" + word.charAt(i))) {
+                    result += "" + word.charAt(i) + c;
 
-            }
+                }
+
         }
         return result;
 
