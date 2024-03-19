16c13cb241549b18da21e47461758d646bedb8ee
Anasben00 <bendwamas@gmail.com>
2024-03-19 01:26:22 -0400

    task2 done

---
+++ src/week07/task2/Array_SortAscending_Anas.java
@@ -0,0 +1,26 @@
+package week07.task2;
+
+import java.util.Arrays;
+
+public class Array_SortAscending_Anas {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(SortAscending(new int []{5,2,3,4,1})));
+    }
+    public static int [] SortAscending(int[] arr){
+
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j < arr.length - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+
+                        int elementAtIndex = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = elementAtIndex;
+                    }
+                }
+            }
+
+
+        return arr;
+    }
+}
