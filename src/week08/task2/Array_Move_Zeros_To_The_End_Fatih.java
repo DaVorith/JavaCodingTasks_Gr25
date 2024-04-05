9d957839c416af20441b0d57dc7f384ced4b5c99
Mefa <fatix.aydin.com>
2024-04-05 10:33:01 -0400

    week 8 task1 and task2

---
+++ src/week08/task2/Array_Move_Zeros_To_The_End_Fatih.java
@@ -0,0 +1,13 @@
+package week08.task2;
+
+public class Array_Move_Zeros_To_The_End_Fatih {
+
+
+}
+
+/*
+Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method)
+Ex:
+input: {1,0,2,0,3,0,4,0};
+output: [1, 2, 3, 4, 0, 0, 0, 0]
+ */
